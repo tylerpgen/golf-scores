db493c7ae6038b97e03ce3840f17e3f7e35a714d
Tyler Gener <tylergen33@gmail.com>
2023-06-13 14:30:12 -0700

    added controllers for editing and deleting scores

--- server/controllers/scoresController.js
+++ server/controllers/scoresController.js
@@ -29,3 +29,32 @@ export const addScore = async (req, res) => {
     res.status(500).json({ message: "Failed to add score" });
   }
 };
+
+// Editing a score by ID
+export const editScore = async (req, res) => {
+  try {
+    const db = await connectToDatabase();
+    const { id } = req.params;
+    const { score } = req.body;
+
+    await Score.findByIdAndUpdate(id, { score });
+    res.json({ message: "Score updated sucessfully" });
+  } catch (error) {
+    console.error("Error updating score:", error);
+    res.status(500).json({ message: "Failed to update score" });
+  }
+};
+
+// Deleting a score by ID
+export const deleteScore = async (req, res) => {
+  try {
+    const db = await connectToDatabase();
+    const { id } = req.params;
+
+    await Score.findByIdAndDelete(id);
+    res.json({ message: "Score deleted!" });
+  } catch (error) {
+    console.error("Error deleting score:", error);
+    res.status(500).json({ message: "Failed to delete score" });
+  }
+};
