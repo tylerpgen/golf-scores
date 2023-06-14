8192227b9916593c4d33c0346134af0cb42a7e34
Tyler Gener <tylergen33@gmail.com>
2023-06-14 13:56:12 -0700

    created signUp controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -27,3 +27,25 @@ export const signIn = async (req, res) => {
     // If an error occurs during the execution, return a 500 response with a generic error message
   }
 };
+
+export const signUp = async (req, res) => {
+  const { email, password, confirmPassword, firstName, lastName } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) return res.status(400).json({ message: "User already exists" });
+
+    if (password !== confirmPassword) return res.status(400).json({ message: "Passwords don't match." });
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}` });
+
+    const token = jwt.sign({ email: result.email, id: result._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+    res.status(200).json({ result: result, token });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong." });
+  }
+};
