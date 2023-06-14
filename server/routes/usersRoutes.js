b5bc6e5c17b643bd1a64867846538c53438c73ec
Tyler Gener <tylergen33@gmail.com>
2023-06-14 14:25:10 -0700

    user route notation

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -3,7 +3,10 @@ import { signIn, signUp } from "../controllers/userController.js";
 
 const router = express.Router();
 
+//Route for sign in
 router.post("/signin", signIn);
+
+//Route for sign up
 router.post("/signup", signUp);
 
 export default router;
