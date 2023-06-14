45039514fc476ba5c7a46a548bded8a68d57cb3b
Tyler Gener <tylergen33@gmail.com>
2023-06-14 14:27:31 -0700

    adding auth to scores routes

--- server/routes/scoresRoutes.js
+++ server/routes/scoresRoutes.js
@@ -1,19 +1,20 @@
 import express from "express";
 import { addScore, editScore, deleteScore } from "../controllers/scoresController.js";
 import { getAllScores } from "../controllers/scoresController.js";
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
 // Route for retreiving all scores
-router.get("/scores", getAllScores);
+router.get("/scores", auth, getAllScores);
 
 //Route for creating a new score
-router.post("/scores/add", addScore);
+router.post("/scores/add", auth, addScore);
 
 //Route for editing an existing score
-router.put("/scores/:id", editScore);
+router.put("/scores/:id", auth, editScore);
 
 //Route for deleting a score
-router.delete("/scores/:id", deleteScore);
+router.delete("/scores/:id", auth, deleteScore);
 
 export default router;
