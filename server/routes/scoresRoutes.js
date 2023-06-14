1fdd0cf2ddce07b61636c7fc87507173d55351d5
Tyler Gener <tylergen33@gmail.com>
2023-06-14 13:22:37 -0700

    notation for routes

--- server/routes/scoresRoutes.js
+++ server/routes/scoresRoutes.js
@@ -4,9 +4,16 @@ import { getAllScores } from "../controllers/scoresController.js";
 
 const router = express.Router();
 
+// Route for retreiving all scores
 router.get("/scores", getAllScores);
+
+//Route for creating a new score
 router.post("/scores", addScore);
+
+//Route for editing an existing score
 router.put("/scores/:id", editScore);
+
+//Route for deleting a score
 router.delete("/scores/:id", deleteScore);
 
 export default router;
