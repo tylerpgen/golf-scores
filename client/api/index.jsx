a35954b8117d4bffe5ff3645fe61b87dbbb5ed45
Tyler Gener <tylergen33@gmail.com>
2023-06-16 12:14:55 -0700

    api notation

--- client/api/index.jsx
+++ client/api/index.jsx
@@ -4,10 +4,12 @@ const api = axios.create({
   baseURL: "http://localhost:5000",
 });
 
+// Posts API
 export const fetchScores = () => api.get("/scores");
 export const addScore = (newScore) => api.post("/scores/add", newScore);
 export const updateScore = (id, updatedScore) => api.put(`/scores/${id}`, updatedScore);
 export const deleteScore = (id) => api.delete(`/scores/${id}`);
 
+//Authentication API
 export const signIn = (formData) => api.post("/user/signin", formData);
 export const signUp = (formData) => api.post("/user/signup", formData);
