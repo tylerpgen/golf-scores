6d04f5f37f16e5def836a1bf5446ff27f1c12f3b
Tyler Gener <tylergen33@gmail.com>
2023-06-16 11:53:14 -0700

    created redux toolkit store and imported reducers

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,13 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
-import thunk from "redux-thunk";
 import App from "./App.jsx";
 import "./index.css";
+import rootReducer from "../reducers";
+
+const store = configureStore({
+  reducer: rootReducer,
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
