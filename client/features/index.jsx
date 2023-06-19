fe12a7d7683150c8a13a57189c384784a56370c4
Tyler Gener <tylergen33@gmail.com>
2023-06-19 16:01:29 -0700

    index.js reducer notation

--- client/features/index.jsx
+++ client/features/index.jsx
@@ -2,9 +2,10 @@ import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "./authReducer";
 import scoresReducer from "./scoresReducer";
 
+// Combining multiple reducers into a root reducer
 const rootReducer = combineReducers({
-  auth: authReducer,
-  scores: scoresReducer,
+  auth: authReducer, // Authentication
+  scores: scoresReducer, // Scores
 });
 
 export default rootReducer;
