9bab197b36b99b215e3a46424e63634dc543ad37
Tyler Gener <tylergen33@gmail.com>
2023-06-19 15:39:07 -0700

    register function created in authReducer

--- client/features/authReducer.jsx
+++ client/features/authReducer.jsx
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import authService from "./authService";
 
 const user = JSON.parse(localStorage.getItem("user"));
 
@@ -9,6 +10,16 @@ const initialState = {
   isLoading: false,
   message: "",
 };
+
+export const register = createAsyncThunk("auth/register", async (user, thunkAPI) => {
+  try {
+    return await authService.register(user);
+  } catch (error) {
+    const message =
+      (error.response && error.response.data && error.response.message) || error.message || error.toString();
+    return thunkAPI.rejectWithValue(message);
+  }
+});
 
 const authSlice = createSlice({
   name: "auth",
