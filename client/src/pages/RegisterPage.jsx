d47b29faca63f34b2df39c7f5d4e61a879f8f75c
Tyler Gener <tylergen33@gmail.com>
2023-06-21 16:15:04 -0700

    created handleSubmit function for form

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -2,6 +2,10 @@ import { useTheme } from "@emotion/react";
 import { Box, Button, Container, FormControl, Paper, TextField, Typography } from "@mui/material";
 
 const RegisterPage = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   const theme = useTheme();
   return (
     <Box
@@ -32,124 +36,126 @@ const RegisterPage = () => {
           >
             CREATE AN ACCOUNT
           </Typography>
-          <FormControl sx={{ display: "flex", flexDirection: "row", padding: "15px" }}>
-            <TextField
-              id="first-name"
-              aria-describedby="first-name"
-              placeholder="First Name"
-              sx={{
-                flex: "1",
-                maxWidth: "48%",
-                marginRight: "10px",
-                "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
-                  border: "1px solid #e8b923",
-                },
-                "& .MuiOutlinedInput-root": {
-                  "&:hover fieldset": {
-                    borderColor: "#005e23",
+          <form onSubmit={handleSubmit}>
+            <FormControl sx={{ display: "flex", flexDirection: "row", padding: "15px" }}>
+              <TextField
+                id="first-name"
+                aria-describedby="first-name"
+                placeholder="First Name"
+                sx={{
+                  flex: "1",
+                  maxWidth: "48%",
+                  marginRight: "10px",
+                  "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
+                    border: "1px solid #e8b923",
                   },
-                },
-              }}
-            />
-            <TextField
-              id="last-name"
-              aria-describedby="last-name"
-              placeholder="Last Name"
-              sx={{
-                flex: "1",
-                minWidth: "40%",
-                "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
-                  border: "1px solid #e8b923",
-                },
-                "& .MuiOutlinedInput-root": {
-                  "&:hover fieldset": {
-                    borderColor: "#005e23",
+                  "& .MuiOutlinedInput-root": {
+                    "&:hover fieldset": {
+                      borderColor: "#005e23",
+                    },
                   },
-                },
-              }}
-            />
-          </FormControl>
-          <FormControl sx={{ display: "flex", flexDirection: "column", padding: "15px" }}>
-            <TextField
-              id="email"
-              aria-describedby="email"
-              placeholder="Email"
-              fullWidth
-              sx={{
-                mb: "15px",
-                "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
-                  border: "1px solid #e8b923",
-                },
-                "& .MuiOutlinedInput-root": {
-                  "&:hover fieldset": {
-                    borderColor: "#005e23",
+                }}
+              />
+              <TextField
+                id="last-name"
+                aria-describedby="last-name"
+                placeholder="Last Name"
+                sx={{
+                  flex: "1",
+                  minWidth: "40%",
+                  "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
+                    border: "1px solid #e8b923",
                   },
-                },
-              }}
-            />
-            <TextField
-              id="password"
-              aria-describedby="password"
-              placeholder="Password"
-              fullWidth
-              sx={{
-                mb: "15px",
-                "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
-                  border: "1px solid #e8b923",
-                },
-                "& .MuiOutlinedInput-root": {
-                  "&:hover fieldset": {
-                    borderColor: "#005e23",
+                  "& .MuiOutlinedInput-root": {
+                    "&:hover fieldset": {
+                      borderColor: "#005e23",
+                    },
                   },
-                },
-              }}
-            />
-            <TextField
-              id="confirm-password"
-              aria-describedby="confirm-password"
-              placeholder="Confirm Password"
-              fullWidth
-              sx={{
-                mb: "15px",
-                "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
-                  border: "1px solid #e8b923",
-                },
-                "& .MuiOutlinedInput-root": {
-                  "&:hover fieldset": {
-                    borderColor: "#005e23",
+                }}
+              />
+            </FormControl>
+            <FormControl sx={{ display: "flex", flexDirection: "column", padding: "15px" }}>
+              <TextField
+                id="email"
+                aria-describedby="email"
+                placeholder="Email"
+                fullWidth
+                sx={{
+                  mb: "15px",
+                  "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
+                    border: "1px solid #e8b923",
                   },
-                },
-              }}
-            />
-            <Typography p="10px" sx={{ fontFamily: "Dosis", fontWeight: "500", fontSize: "1rem" }}>
-              By creating an account, I agree to the terms of service of the site, in accordance to the{" "}
-              <b>PRIVACY POLICY</b>
-            </Typography>
-            <Button
-              type="submit"
-              variant="contained"
-              sx={{
-                marginTop: "10px",
-                padding: "15px",
-                fontSize: "1.4rem",
-                fontFamily: "Dosis",
-                fontWeight: "600",
-                height: "50px",
-                backgroundColor: "#e8b923",
-                "&:hover": {
+                  "& .MuiOutlinedInput-root": {
+                    "&:hover fieldset": {
+                      borderColor: "#005e23",
+                    },
+                  },
+                }}
+              />
+              <TextField
+                id="password"
+                aria-describedby="password"
+                placeholder="Password"
+                fullWidth
+                sx={{
+                  mb: "15px",
+                  "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
+                    border: "1px solid #e8b923",
+                  },
+                  "& .MuiOutlinedInput-root": {
+                    "&:hover fieldset": {
+                      borderColor: "#005e23",
+                    },
+                  },
+                }}
+              />
+              <TextField
+                id="confirm-password"
+                aria-describedby="confirm-password"
+                placeholder="Confirm Password"
+                fullWidth
+                sx={{
+                  mb: "15px",
+                  "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
+                    border: "1px solid #e8b923",
+                  },
+                  "& .MuiOutlinedInput-root": {
+                    "&:hover fieldset": {
+                      borderColor: "#005e23",
+                    },
+                  },
+                }}
+              />
+              <Typography p="10px" sx={{ fontFamily: "Dosis", fontWeight: "500", fontSize: "1rem" }}>
+                By creating an account, I agree to the terms of service of the site, in accordance to the{" "}
+                <b>PRIVACY POLICY</b>
+              </Typography>
+              <Button
+                type="submit"
+                variant="contained"
+                sx={{
+                  marginTop: "10px",
+                  padding: "15px",
+                  fontSize: "1.4rem",
+                  fontFamily: "Dosis",
+                  fontWeight: "600",
+                  height: "50px",
                   backgroundColor: "#e8b923",
-                  transform: "scale(1.02)",
-                  transition: "all 0.1s ease-in-out",
-                },
-                [theme.breakpoints.up("lg")]: {
-                  fontSize: "2rem",
-                  width: "fit-content",
-                },
-              }}
-            >
-              Create Account
-            </Button>
-          </FormControl>
+                  "&:hover": {
+                    backgroundColor: "#e8b923",
+                    transform: "scale(1.02)",
+                    transition: "all 0.1s ease-in-out",
+                  },
+                  [theme.breakpoints.up("lg")]: {
+                    fontSize: "2rem",
+                    width: "fit-content",
+                  },
+                }}
+              >
+                Create Account
+              </Button>
+            </FormControl>
+          </form>
         </Paper>
       </Container>
     </Box>
