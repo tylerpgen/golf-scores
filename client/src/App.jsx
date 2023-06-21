1fc924c943404e9d2909a7dc3d6dac04095195ed
Tyler Gener <tylergen33@gmail.com>
2023-06-21 15:06:25 -0700

    changed bg color added sign up button and font

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import { ThemeProvider } from "@emotion/react";
 import { createTheme } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 
-const theme = createTheme();
+const theme = createTheme({});
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Router>
         <Routes>
           <Route path="/" exact element={<HomePage />} />
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,27 +1,83 @@
 import { useTheme } from "@emotion/react";
-import { Typography, Container } from "@mui/material";
+import { Typography, Container, Box, Button, Link } from "@mui/material";
 
 const HomePage = () => {
   const theme = useTheme();
   return (
     // Landing Page
-    <Container maxWidth="lg">
-      <Typography
-        align="center"
-        variant="h1"
-        sx={{
-          mb: "8px",
-          fontSize: "4rem",
-          fontFamily: "Dancing Script",
-          fontWeight: "700",
-          [theme.breakpoints.up("lg")]: {
-            fontSize: "10rem",
-          },
-        }}
-      >
-        Golf Scores
-      </Typography>
-    </Container>
+    <Box
+      sx={{
+        minHeight: "100vh",
+        backgroundColor: "#005e23",
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
+      <Container maxWidth="lg" sx={{ paddingBottom: "150px" }}>
+        <Typography
+          align="center"
+          variant="h1"
+          sx={{
+            mb: "8px",
+            // pt: "16px",
+            color: "white",
+            fontSize: "4rem",
+            fontFamily: "Dancing Script",
+            fontWeight: "700",
+            [theme.breakpoints.up("lg")]: {
+              fontSize: "10rem",
+            },
+          }}
+        >
+          Golf Scores
+        </Typography>
+        <Typography
+          align="center"
+          variant="h2"
+          color="white"
+          sx={{
+            fontSize: "1.7rem",
+            fontFamily: "Dosis",
+            fontWeight: "500",
+            [theme.breakpoints.up("lg")]: {
+              fontSize: "2.6rem",
+              marginTop: "10px",
+            },
+          }}
+        >
+          Keep track of your scores easily!
+        </Typography>
+        <Container align="center">
+          <Link href="/signup" sx={{ textDecoration: "none" }}>
+            <Button
+              variant="contained"
+              sx={{
+                marginTop: "20px",
+                fontSize: "1.4rem",
+                fontFamily: "Dosis",
+                fontWeight: "600",
+                height: "50px",
+                width: "fit-content",
+                backgroundColor: "#e8b923",
+
+                "&:hover": {
+                  backgroundColor: "#e8b923",
+                  transform: "scale(1.1)",
+                  transition: "all 0.1s ease-in-out",
+                },
+                [theme.breakpoints.up("lg")]: {
+                  fontSize: "2rem",
+                  width: "250px",
+                },
+              }}
+            >
+              Sign UP Now
+            </Button>
+          </Link>
+        </Container>
+      </Container>
+    </Box>
   );
 };
 
