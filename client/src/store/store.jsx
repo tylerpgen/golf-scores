51aeb26d6d52238302705de4e257484e817545a5
Tyler Gener <tylergen33@gmail.com>
2023-06-25 15:24:33 -0700

    moved features to src folder

--- client/src/store/store.jsx
+++ client/src/store/store.jsx
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "../../features";
+import rootReducer from "../features";
 
 const store = configureStore({
   reducer: rootReducer,
