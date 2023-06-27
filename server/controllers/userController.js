69aabd819513360beebcd6935e420b4d21fba2c0
Tyler Gener <tylergen33@gmail.com>
2023-06-27 11:22:04 -0700

    controller for creating user added with error

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -21,7 +21,22 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("User already exists");
   }
 
-  res.status(200).json({ message: "Register User" });
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid User Data");
+  }
 });
 
 //@desc Logout user
