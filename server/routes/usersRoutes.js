8038f3590e0758dce101b2f7347cb180c94e31eb
Tyler Gener <tylergen33@gmail.com>
2023-06-26 18:47:10 -0700

    fixed bug that was preventing userRegister post

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -9,7 +9,7 @@ import {
 
 const router = express.Router();
 
-router.post("/", authUser);
+router.post("/", registerUser);
 router.post("/auth", authUser);
 router.post("/logout", logoutUser);
 router.route("/profile").get(getUserProfile).put(updateUserProfile);
