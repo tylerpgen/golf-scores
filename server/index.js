88cf5352ebcaaf6c1083ad1102f3d4b169605825
Tyler Gener <tylergen33@gmail.com>
2023-06-26 18:28:51 -0700

    added middleware error handlers to index

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 // import { connectToDatabase } from "./database/db.js";
 import userRoutes from "./routes/usersRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 dotenv.config();
 
 const app = express();
@@ -11,5 +12,8 @@ const port = process.env.PORT || 5000;
 app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => res.send("Server is ready"));
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is started on port ${port}`));
