0ddcbda5475f9a349ba5f2d6b7985b35b8cc646a
Tyler Gener <tylergen33@gmail.com>
2023-06-26 18:31:23 -0700

    fixed typo in error handler

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,6 +6,8 @@ import asyncHandler from "express-async-handler";
 //@desc Auth user/set token
 //route POST /api/users/auth
 const authUser = asyncHandler(async (req, res) => {
+  res.status(401);
+  throw new Error("Something went wrong");
   res.status(200).json({ message: "Auth User" });
 });
 
--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -6,7 +6,7 @@ const notFound = (req, res, next) => {
 
 const errorHandler = (err, req, res, next) => {
   let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  let message = error.message;
+  let message = err.message;
 
   if (err.name === "CastError" && err.kind === "ObjectId") {
     statusCode = 404;
