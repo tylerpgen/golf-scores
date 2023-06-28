d00524d77ac9ccc510e2659b7204cea346d7cb9a
Tyler Gener <tylergen33@gmail.com>
2023-06-28 14:13:53 -0700

    created getUserProfile function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -67,7 +67,12 @@ const logoutUser = asyncHandler(async (req, res) => {
 //@desc Get user
 //route POST /api/users/profile
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get User Profile" });
+  const user = {
+    _id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  };
+  res.status(200).json(user);
 });
 
 //@desc Update user
