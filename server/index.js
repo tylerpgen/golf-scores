c1ca775f1f45ec2509e882c51db5deceb0489e8f
Tyler Gener <tylergen33@gmail.com>
2023-06-28 14:08:03 -0700

    created auth middleware to protect routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import connectDB from "./database/db.js";
 import userRoutes from "./routes/usersRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
@@ -13,6 +14,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => res.send("Server is ready"));
