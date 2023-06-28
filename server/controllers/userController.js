db7ea31376e89596a4e049283a88402b7dbfba0f
Tyler Gener <tylergen33@gmail.com>
2023-06-28 13:56:19 -0700

    method to auth function to call matchPassword

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -9,7 +9,7 @@ const authUser = asyncHandler(async (req, res) => {
 
   const user = await User.findOne({ email });
 
-  if (user) {
+  if (user && (await user.matchPassword(password))) {
     generateToken(res, user._id);
     res.status(201).json({
       _id: user._id,
