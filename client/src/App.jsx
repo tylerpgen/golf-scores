a5e7f5e752707eac7f440215c394eba96a20648f
Tyler Gener <tylergen33@gmail.com>
2023-06-30 13:08:59 -0700

    updated react router dom routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,23 +3,14 @@ import { createTheme } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import RegisterPage from "./pages/RegisterPage";
-import LoginPage from "./pages/LoginPage";
+import { Outlet } from "react-router-dom";
 const theme = createTheme({});
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Router>
-        <Routes>
-          <Route path="/" exact element={<HomePage />} />
-          <Route path="/register" exact element={<RegisterPage />} />
-          <Route path="/login" exact element={<LoginPage />} />
-        </Routes>
-      </Router>
+      <Outlet />
       <ToastContainer />
     </ThemeProvider>
   );
