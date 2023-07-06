b44b99043d6bf4f15d4f8c251fad825bdcad7b74
Tyler Gener <tylergen33@gmail.com>
2023-07-05 18:44:51 -0700

    store.jsx notation

--- client/src/store/store.jsx
+++ client/src/store/store.jsx
@@ -2,13 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authReducer";
 import { apiSlice } from "../features/apiSlices";
 
+// Configure the Redux store
 const store = configureStore({
   reducer: {
-    auth: authReducer,
-    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer, // Add the authReducer to the store
+    [apiSlice.reducerPath]: apiSlice.reducer, // Add the apiSlice reducer to the store
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
-  devTools: true,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware), // Add the apiSlice middleware to the store's middleware stack
+  devTools: true, // Enable Redux DevTools extension
 });
 
 export default store;
