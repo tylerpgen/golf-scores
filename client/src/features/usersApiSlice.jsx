4f707d8798fbd98a7490b7683adb4fb4b377bfa7
Tyler Gener <tylergen33@gmail.com>
2023-07-05 18:42:28 -0700

    usersApiSlice notation

--- client/src/features/usersApiSlice.jsx
+++ client/src/features/usersApiSlice.jsx
@@ -1,9 +1,11 @@
 import { apiSlice } from "./apiSlices";
 const USERS_URL = "/api/users";
 
+// Set up the usersApiSlice
 export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     login: builder.mutation({
+      // Define the login mutation
       query: (data) => ({
         url: `${USERS_URL}/auth`,
         method: "POST",
@@ -13,4 +15,5 @@ export const usersApiSlice = apiSlice.injectEndpoints({
   }),
 });
 
+// Extract the useLoginMutation from the usersApiSlice
 export const { useLoginMutation } = usersApiSlice;
