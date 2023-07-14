b9ab7e4e3c2c9d89deac0208ddd3396623ae255c
Tyler Gener <tylergen33@gmail.com>
2023-07-14 12:20:38 -0700

    scores routes created with protect middlware

--- server/routes/scoresRoutes.js
+++ server/routes/scoresRoutes.js
@@ -1,20 +1,19 @@
-// import express from "express";
-// import { addScore, editScore, deleteScore } from "../controllers/scoresController.js";
-// import { getAllScores } from "../controllers/scoresController.js";
-// import auth from "../middleware/auth.js";
+import express from "express";
+import { getAllScores, addScore, updateScore, deleteScore } from "../controllers/scoresController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
-// const router = express.Router();
+const router = express.Router();
 
-// // Route for retreiving all scores
-// router.get("/scores", auth, getAllScores);
+// Route for retreiving all scores
+router.get("/scores", protect, getAllScores);
 
-// //Route for creating a new score
-// router.post("/scores/add", auth, addScore);
+//Route for creating a new score
+router.post("/scores/add", protect, addScore);
 
-// //Route for editing an existing score
-// router.put("/scores/:id", auth, editScore);
+//Route for editing an existing score
+router.put("/scores/:id", protect, updateScore);
 
-// //Route for deleting a score
-// router.delete("/scores/:id", auth, deleteScore);
+//Route for deleting a score
+router.delete("/scores/:id", protect, deleteScore);
 
-// export default router;
+export default router;
