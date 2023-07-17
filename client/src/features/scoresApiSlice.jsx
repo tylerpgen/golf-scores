5929a214a3bc0a06c7a2914a0ee5933d73e814b8
Tyler Gener <tylergen33@gmail.com>
2023-07-17 16:19:14 -0700

    created scoresApi slice and reconfigured routes

---
+++ client/src/features/scoresApiSlice.jsx
@@ -0,0 +1,20 @@
+import { apiSlice } from "./apiSlices";
+const USERS_URL = "/api/users";
+
+export const scoresApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getScores: builder.mutation({
+      query: () => ({
+        url: `${USERS_URL}/scores`,
+        method: "GET",
+      }),
+    }),
+    createScore: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/scores/add`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
--- server/models/scores.js
+++ server/models/scores.js
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
 
-const scoreSchema = new mongoose.Schema({
-  course: { type: String, required: true },
-  date: { type: Date, required: true },
-  score: { type: Number, required: true },
-});
+const scoreSchema = new mongoose.Schema(
+  {
+    course: { type: String, required: true },
+    date: { type: String, required: true },
+    score: { type: Number, required: true },
+    id: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export const Score = mongoose.model("Score", scoreSchema);
