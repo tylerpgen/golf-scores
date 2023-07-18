a400adb1a7987265bc050756304aca69597c0607
Tyler Gener <tylergen33@gmail.com>
2023-07-18 10:42:20 -0700

    created score slice and imported to redux store

--- client/src/store/store.jsx
+++ client/src/store/store.jsx
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authReducer";
 import { apiSlice } from "../features/apiSlices";
+import scoreReducer from "../features/scoreSlice";
 
 // Configure the Redux store
 const store = configureStore({
   reducer: {
     auth: authReducer, // Add the authReducer to the store
     [apiSlice.reducerPath]: apiSlice.reducer, // Add the apiSlice reducer to the store
+    scores: scoreReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware), // Add the apiSlice middleware to the store's middleware stack
   devTools: true, // Enable Redux DevTools extension
