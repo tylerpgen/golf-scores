9dd7452604765f6647a7451bdc6083596686ad7e
Tyler Gener <tylergen33@gmail.com>
2023-07-20 00:59:46 -0700

    setting up server for deployment

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import connectDB from "./database/db.js";
@@ -19,7 +20,14 @@ app.use(cookieParser());
 app.use("/api/users/scores", scoresRoutes);
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => res.send("Server is ready"));
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "..", "client", "dist")));
+
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "..", "client", "dist", "index.html")));
+} else {
+  app.get("/", (req, res) => res.send("Server is ready"));
+}
 
 app.use(notFound);
 app.use(errorHandler);
