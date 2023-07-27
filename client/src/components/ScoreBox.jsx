20c9612e2b874d56bb08fabe938c61a2fca7e87a
Tyler Gener <tylergen33@gmail.com>
2023-07-27 11:13:17 -0700

    score box component notation

--- client/src/components/ScoreBox.jsx
+++ client/src/components/ScoreBox.jsx
@@ -1,12 +1,18 @@
+// Import necessary libraries and components
 import { useTheme } from "@emotion/react";
 import { Container, Paper, Typography, IconButton } from "@mui/material";
 import PropTypes from "prop-types";
 import DeleteIcon from "@mui/icons-material/Delete";
 
+// ScoreBox Component
 const ScoreBox = ({ course, date, score, scoreId, handleDeleteClick }) => {
+  // Get the current theme using the useTheme hook from @emotion/react
   const theme = useTheme();
+
+  // Return JSX for rendering the ScoreBox component
   return (
     <Container maxWidth="md" sx={{ padding: "15px" }}>
+      {/* Paper component representing the score box */}
       <Paper
         elevation={5}
         sx={{
@@ -16,13 +22,15 @@ const ScoreBox = ({ course, date, score, scoreId, handleDeleteClick }) => {
           flexDirection: "column",
           alignItems: "center",
           [theme.breakpoints.up("lg")]: {
-            flexDirection: "row",
+            flexDirection: "row", // Switch to row direction for large screens using theme breakpoints
           },
         }}
       >
+        {/* Course Name */}
         <Typography variant="h5" fontFamily="Dosis" fontWeight="700">
           Course Name:
         </Typography>
+        {/* Display the name of the course */}
         <Typography
           variant="h6"
           sx={{
@@ -30,43 +38,49 @@ const ScoreBox = ({ course, date, score, scoreId, handleDeleteClick }) => {
             fontWeight: "00",
             my: "8px",
             [theme.breakpoints.up("lg")]: {
-              my: "0",
+              my: "0", // Remove margin for large screens using theme breakpoints
             },
           }}
         >
           {course}
         </Typography>
+        {/* Date */}
         <Typography variant="h5" fontFamily="Dosis" fontWeight="700">
           Date:
         </Typography>
+        {/* Display the date */}
         <Typography
           variant="h6"
           sx={{
             fontFamily: "Dosis",
             my: "8px",
             [theme.breakpoints.up("lg")]: {
-              my: "0",
+              my: "0", // Remove margin for large screens using theme breakpoints
             },
           }}
         >
           {date}
         </Typography>
+        {/* Score */}
         <Typography variant="h5" fontFamily="Dosis" fontWeight="700">
           Score:
         </Typography>
+        {/* Display the score */}
         <Typography
           variant="h6"
           sx={{
             fontFamily: "Dosis",
             my: "8px",
             [theme.breakpoints.up("lg")]: {
-              my: "0",
+              my: "0", // Remove margin for large screens using theme breakpoints
             },
           }}
         >
           {score}
         </Typography>
+        {/* Delete Icon Button */}
         <IconButton onClick={() => handleDeleteClick(scoreId)} sx={{ color: "#e8b923" }}>
+          {/* Render the delete icon from MUI icons */}
           <DeleteIcon />
         </IconButton>
       </Paper>
@@ -74,6 +88,7 @@ const ScoreBox = ({ course, date, score, scoreId, handleDeleteClick }) => {
   );
 };
 
+// Define PropTypes for type checking of props
 ScoreBox.propTypes = {
   course: PropTypes.string.isRequired,
   date: PropTypes.string.isRequired,
@@ -82,4 +97,5 @@ ScoreBox.propTypes = {
   handleDeleteClick: PropTypes.func.isRequired,
 };
 
+// Export the ScoreBox component
 export default ScoreBox;
