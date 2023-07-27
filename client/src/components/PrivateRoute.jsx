40e6920f40e2029365bcca6546f9ea4df065d66e
Tyler Gener <tylergen33@gmail.com>
2023-07-27 11:11:50 -0700

    private route middleware notation

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -3,6 +3,9 @@ import { useSelector } from "react-redux";
 
 const PrivateRoute = () => {
   const { userInfo } = useSelector((state) => state.auth);
+
+  // Check if the user is authenticated (userInfo is not null)
+  // If the user is authenticated, render the child components
   return userInfo ? <Outlet /> : <Navigate to="/login" replace />;
 };
 
