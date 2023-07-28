172e9acbaa9238363c7619a8bc8f994df363a8a1
Tyler Gener <tylergen33@gmail.com>
2023-07-28 13:13:08 -0700

    server db notation

--- server/database/db.js
+++ server/database/db.js
@@ -1,18 +1,25 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
 
+// Get the MongoDB connection URI from the environment variables
 const uri = process.env.MONGODB_URI;
 
 // Function to connect to the MongoDB database
 const connectDB = async () => {
   try {
+    // Attempt to connect to the MongoDB database using the provided URI
     const conn = await mongoose.connect(uri);
+
+    // If the connection is successful, log the message with the connected host
     console.log(`MongoDB Connected ${conn.connection.host}`);
   } catch (error) {
+    // If an error occurs during the connection attempt, log the error message
     console.error(`Error: ${error.message}`);
-    process.exit(1); // If an error occurs, exit the Node.js process with a non-zero exit code
+
+    // Exit the Node.js process with a non-zero exit code (1) to indicate failure
+    process.exit(1);
   }
 };
 
